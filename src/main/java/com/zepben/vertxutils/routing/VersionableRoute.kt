42a365755e7cc550697b10ab6cd1470e14e96aff
Kurt Greaves <kurt.greaves@zepben.com>
2024-11-15 12:23:33 +1100

    Add a Kotlin file to allow dokka to build a doc jar.
    Signed-off-by: Kurt Greaves <kurt.greaves@zepben.com>

--- src/main/java/com/zepben/vertxutils/routing/VersionableRoute.kt
+++ src/main/java/com/zepben/vertxutils/routing/VersionableRoute.kt
@@ -5,14 +5,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+package com.zepben.vertxutils.routing
 
-package com.zepben.vertxutils.routing;
+import com.zepben.annotations.EverythingIsNonnullByDefault
 
-import com.zepben.annotations.EverythingIsNonnullByDefault;
-
+/**
+ * It's a versionable Route
+ */
 @EverythingIsNonnullByDefault
-public interface VersionableRoute {
-
-    RouteVersion routeVersion();
-
+interface VersionableRoute {
+    fun routeVersion(): RouteVersion?
 }
